d24b893cbddec2523bbd66b8a8fc9316031ce593
chatsuite <40656068+chatsuite@users.noreply.github.com>
2018-06-28 12:03:34 +0300

    Create chat-gui-builder.js

---
+++ chat-gui-builder.js
@@ -0,0 +1,80 @@
+/*!
+ * chat-gui-builder.js - ChatGUIBuilder JS v1.0.0
+ * ---------------------------------------------------------
+ * NPM:    npmjs.com/package/chat-gui-builder
+ * GitHub: github.com/chatsuite/chat-gui-builder
+ * ---------------------------------------------------------
+ * Author: Ron Bar ^ Asaf Yannay
+ * Email:  ron@chatsuit.com
+ * Www:    chatsuite.com
+ * GitHub: github.com/chatsuite
+ * ---------------------------------------------------------
+ * 19 JUL 2018
+ */
+
+
+var ChatGUIBuilder = function () {
+	this.SELF_CLOSING_TAGS = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr']
+	
+	this.body = '';
+	this.tags = [];
+};
+
+ChatGUIBuilder.prototype.toString = function () {
+	return this.body;
+};
+
+ChatGUIBuilder.prototype.parse = function (document) {
+	var wrapper = document.createElement('div');
+	wrapper.innerHTML = this.body;
+
+	return wrapper.firstChild;
+};
+
+ChatGUIBuilder.prototype.ele = function (tag, arg2, arg3) {
+	this.body = this.body.concat('<'+tag);
+
+	if (typeof arg2 == 'object') {
+		for (let attr in arg2) {
+            if(attr === 'data'){
+                for(let dataAttr of Object.keys(arg2.data)){
+                    this.body = this.body.concat(' data-'+dataAttr+'='+'"'+arg2.data[dataAttr]+'"');
+                }
+            }
+            else {
+                this.body = this.body.concat(' '+attr+'='+'"'+arg2[attr]+'"');
+            }
+        }
+	}
+
+	if (this.SELF_CLOSING_TAGS.indexOf(tag) == -1) {
+		this.body = this.body.concat('>');
+
+		if (typeof arg2 == 'string') {
+			this.body = this.body.concat(arg2).concat('</'+tag+'>');
+		} else if (typeof arg3 == 'string') {
+			this.body = this.body.concat(arg3).concat('</'+tag+'>');
+		} else {
+			this.tags.push(tag);
+		}
+
+	} else {
+		this.body = this.body.concat('/>');
+	}
+	
+	return this;
+};
+
+ChatGUIBuilder.prototype.val = function (text) {
+	this.body = this.body.concat(text);
+	return this;
+};
+
+ChatGUIBuilder.prototype.cl = function () {
+	this.body = this.body.concat('</'+this.tags.pop()+'>');
+	return this;
+};
+
+if ( typeof module === "object" && typeof module.exports === "object" ) {
+	module.exports = ChatGUIBuilder;
+}
